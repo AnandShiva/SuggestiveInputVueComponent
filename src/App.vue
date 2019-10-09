b4a1d5c20324451f1c18dfeb39e6536fedf105c5
AnandShiva <anandkumar.shiva@gmail.com>
2019-10-10 03:36:55 +0530

    SuggestiveInput  Base setup

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app">
+    <SuggestiveInput :suggestionProps="suggestionInp"/>
+    <br>
+    
+    <SuggestiveInput :suggestionProps="suggestionInp" />
+
+  </div>
+</template>
+
+<script>
+import SuggestiveInput from './components/SuggestiveInput.vue';
+
+export default {
+  name: 'app',
+  components: {
+    SuggestiveInput
+  }, 
+  data(){
+    return {
+      suggestionInp : [
+        "abc",
+        "def",
+        "ghi",
+        "fjk",
+        "lmo",
+        "pqr",
+        "stu",
+        "vwx",
+        "yz"
+      ]
+    };
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SuggestiveInput.vue
@@ -0,0 +1,112 @@
+<template>
+  <div>
+    <div class="suggestiveInput">
+      <input type="text" v-model="currentUserInput" />
+      <ul v-show="showPopUp" class="popup"> 
+        <li class="list_item"
+          @click="suggestionItemSelected(item)"
+          v-bind:key="index+200"
+          v-html="highLightMatchingSection(currentUserInput, item)"
+          v-for="(item,index) in matchingSuggestionItems"
+        ></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+input {
+  padding: 10px;
+  display: block;
+  background-color: inherit;
+}
+ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+.popup {
+  position: absolute;
+  visibility: hidden;
+  width: 100%;
+}
+.popup.show {
+  visibility: visible;
+  cursor: pointer;
+  background-color: #ffffff ;
+  z-index: 10;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+.suggestiveInput {
+  position: relative;
+  width: fit-content;
+}
+
+.list_item{
+  padding-left: 1em;
+}
+
+.list_item:hover{
+  background-color: #d6d6d6;
+  text-align: start;
+  border : solid;
+  border-width: 3px;
+  border-color: #d6d6d600;
+}
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      showPopUp: false,
+      currentUserInput: ""
+    };
+  },
+  props: ["suggestionProps"],
+  watch: {
+    currentUserInput: function() {
+      let currentInput = this.currentUserInput;
+      let popup = this.$el.getElementsByTagName("ul")[0];
+      if (this.showPopUp) {
+        popup.classList.add("show");
+      } else {
+        popup.classList.remove("show");
+      }
+    }
+  },
+  computed: {
+    matchingSuggestionItems: function() {
+      this.showPopUp = true;
+      let currentInput = this.currentUserInput;
+      let suggestedItems = this.$props.suggestionProps.filter(items => {
+        return items.indexOf(currentInput) != -1;
+      });
+      return suggestedItems;
+    },
+  },
+  methods: {
+    suggestionItemSelected(itemText) {
+      this.currentUserInput = itemText;
+      this.showPopUp = false;
+    },
+    highLightMatchingSection(currentUserInput, suggestedItem){
+      let matchingSectionIndex =-1;
+      if(currentUserInput != ""){
+        matchingSectionIndex = suggestedItem.indexOf(currentUserInput);
+      }
+      if(matchingSectionIndex == -1){
+        return suggestedItem;
+      }
+      let highlightedHtmlString = "";
+      highlightedHtmlString = suggestedItem.substring(0,matchingSectionIndex);
+      highlightedHtmlString += "<b>";
+      highlightedHtmlString += currentUserInput + "</b>";
+      highlightedHtmlString += suggestedItem.substring(matchingSectionIndex+currentUserInput.length);
+      return highlightedHtmlString;
+      
+    }
+  }
+};
+</script>
